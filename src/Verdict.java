556571bff44d4c91c05402923995ba467b06aa0d
JackyChen343 <343089579@tdsb.ca>
2026-05-14 11:31:50 -0400

    Update Verdict.java

--- src/Verdict.java
+++ src/Verdict.java
@@ -8,5 +8,39 @@
  * @author 343089579
  */
 public class Verdict {
+    private String caseTitle;
+    private String studentVerdict;
+    private String reason;
     
+    public Verdict(){
+        this.caseTitle = "none";
+        this.studentVerdict = "none";
+        this.reason = "none";
+    }
+    public Verdict(String caseTitle, String verdict, String reason){
+        this.caseTitle = caseTitle;
+        this.studentVerdict = verdict;
+        this.reason = reason;
+    }
+    public void setCase(String caseTitle){
+        this.caseTitle = caseTitle;
+    }
+    public String getCase(){
+        return caseTitle;
+    }
+    public void setReason(String reason){
+        this.reason = reason;
+    }
+    public String getReason(){
+        return reason;
+    }
+    public void setStudentVerdict(String verdict){
+        this.studentVerdict = verdict; 
+    }
+    public String getStudentVerdict(){
+        return studentVerdict;
+    }
+    public String toString(){
+        return "Case Title: " + caseTitle + " Student Verdict: " + studentVerdict + " Reason: " + reason;
+    }
 }
