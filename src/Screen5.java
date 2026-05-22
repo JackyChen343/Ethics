47ecd56461aacbdbf0f25bd474b52214517d290b
343089579 <343089579@C-G5JWJB2.tdsb.on.ca>
2026-05-22 11:09:07 -0400

    GSDSDGGDSDG SDG AG A AG AS GAS GASG GS A dfg sg sd gsd  gdsg sdgd sgd sgd gd ssdg sgd sgd gsdgssdg ggdgsd sgsdg gsd gds gdsg sd gsd gsdg d sg sdg dsg dsg dsgsd wete42  45rgrfdfd 45 trdftd  34  d g53g3 fd g34t3 3 3 g3  gd df  34er f g34 hrg3 4y3ygf  yuge 8r 6t8er 8g 87tgr8

--- src/Screen5.java
+++ src/Screen5.java
@@ -4,6 +4,9 @@
  */
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
@@ -21,7 +24,9 @@ public class Screen5 extends javax.swing.JFrame {
             public void windowActivated(WindowEvent e) {
                 System.out.println("JFrame is now focused!");
                 //add code here
-                
+                jLabel1.setText(Mainmenu.cases[4].getCaseTitle());
+                jLabel2.setText(Mainmenu.cases[4].getCategory());
+                jTextArea1.setText(Mainmenu.cases[4].getCaseDescription());
             }
         });
     }
@@ -47,7 +52,6 @@ public class Screen5 extends javax.swing.JFrame {
         jRadioButton1 = new javax.swing.JRadioButton();
         jRadioButton2 = new javax.swing.JRadioButton();
         jLabel3 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -78,13 +82,6 @@ public class Screen5 extends javax.swing.JFrame {
 
         jLabel3.setText("Reason:");
 
-        jButton2.setText("Questions");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -92,10 +89,7 @@ public class Screen5 extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1))
+                    .addComponent(jButton1)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 532, Short.MAX_VALUE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
@@ -125,21 +119,12 @@ public class Screen5 extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                .addComponent(jButton1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        jLabel1.setText(Mainmenu.cases[4].getCaseTitle());
-        jLabel2.setText(Mainmenu.cases[4].getCategory());
-        jTextArea1.setText(Mainmenu.cases[4].getCaseDescription());
-    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
@@ -148,6 +133,17 @@ public class Screen5 extends javax.swing.JFrame {
         } else if (jRadioButton2.isSelected()){
             Mainmenu.unethic++;
         }
+        
+        String Reason = jTextArea2.getText();
+        try{
+            FileWriter w = new FileWriter("verdict.txt", true);
+            PrintWriter Out = new PrintWriter(w);
+            Out.print(Reason + ", ");
+            Out.close();
+        }catch(IOException ioException){
+            System.err.println("Java Exception: "+ ioException);
+        }
+        
         new Screen6().setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
@@ -190,7 +186,6 @@ public class Screen5 extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
